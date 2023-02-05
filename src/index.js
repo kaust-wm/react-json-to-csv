9fb722c23c3d9049d686284814cde8fcd4dd6417
Kaust <106951363+kaust-wm@users.noreply.github.com>
2023-02-05 12:56:04 +0530

    added columnsToIgnore feature passing this array will not render the mentioned columns

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react'
 import csvDownload from 'json-to-csv-export'
 
 const CsvDownloadButton = (props) => {
-  const { data, filename, delimiter, headers, ...others } = props
+  const { data, filename, delimiter, headers, columnsToIgnore, ...others } = props
 
+  function removeColumns(objects, columnsToIgnore) {
+    return objects.map(obj => {
+      const newObj = {};
+      Object.entries(obj).forEach(([key, value]) => {
+        if (!columnsToIgnore.includes(key)) {
+          newObj[key] = value;
+        }
+      });
+      return newObj;
+    });
+  }
+
+  const filteredData = columnsToIgnore ? removeColumns(data, columnsToIgnore) : data;
   return (
     <button
-      onClick={() => csvDownload({ data, filename, delimiter, headers })}
+      onClick={() => csvDownload({ data: filteredData, filename, delimiter, headers })}
       {...others}
     >
       {props.children || 'Download Data'}
